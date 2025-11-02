f6068864dad3ed33205c1588f5a16e5423203f09
bopry <bopry6@gmail.com>
2025-11-03 00:01:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const buttons = document.querySelectorAll('#top-area div');
 
+const alert = document.querySelector(".aside-settings");
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     button.classList.toggle('animate');
+    alert.style.transform = "translate(50%,50%)";
     setTimeout( () => {
         button.classList.toggle('hidden');
     }, 300)
   });
 });
+
+
+alert
